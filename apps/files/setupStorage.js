eee269e491adda47a426e1e725541b44ae66aafc
Yasef Hatam <hatamyasef@gmail.com>
2025-08-15 16:43:43 +0700

    Update setupStorage.js

--- apps/files/setupStorage.js
+++ apps/files/setupStorage.js
@@ -1,20 +1,11 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 
-const STORAGE_PATH = path.join(__dirname, '../../storage');
+const STORAGE_PATH = path.join(__dirname, 'storage');
 
-beforeAll(() => {
-  if (!fs.existsSync(STORAGE_PATH)) fs.mkdirSync(STORAGE_PATH);
+if (!fs.existsSync(STORAGE_PATH)) {
+  fs.mkdirSync(STORAGE_PATH, { recursive: true });
+  console.log(`Created storage folder at ${STORAGE_PATH}`);
+}
 
-  const dummyFile = path.join(STORAGE_PATH, 'dummy.jpg');
-  if (!fs.existsSync(dummyFile)) {
-    fs.writeFileSync(dummyFile, 'dummy content');
-  }
-});
-
-afterAll(() => {
-  const dummyFile = path.join(STORAGE_PATH, 'dummy.jpg');
-  if (fs.existsSync(dummyFile)) fs.unlinkSync(dummyFile);
-});
+module.exports = STORAGE_PATH;
